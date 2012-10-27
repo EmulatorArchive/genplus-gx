7c9cd0018b18c2230ec00fe2c4f841bcd832a2ef
ekeeke31 <ekeeke31@fcb46045-4337-0410-a722-cd382f7ce8f6>
2012-10-27 14:33:15 +0000

    synced libretro port with github repro

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-GENPLUS_SRC_DIR := ../src
-LIBRETRO_DIR	:= ../libretro
+GENPLUS_SRC_DIR := ../../source
+LIBRETRO_DIR	:= ../
 
-LOCAL_MODULE    := libretro
+LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(GENPLUS_SRC_DIR)/genesis.c \
 			$(GENPLUS_SRC_DIR)/vdp_ctrl.c \
 			$(GENPLUS_SRC_DIR)/vdp_render.c \
@@ -26,31 +26,40 @@ LOCAL_SRC_FILES := $(GENPLUS_SRC_DIR)/genesis.c \
 			$(GENPLUS_SRC_DIR)/input_hw/teamplayer.c \
 			$(GENPLUS_SRC_DIR)/input_hw/xe_a1p.c \
 			$(GENPLUS_SRC_DIR)/input_hw/terebi_oekaki.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/cd_cart.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/cdc.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/cdd.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/gfx.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/pcm.c \
+			$(GENPLUS_SRC_DIR)/cd_hw/scd.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/areplay.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/md_cart.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/sms_cart.c \
-			$(GENPLUS_SRC_DIR)/cart_hw/gg_eeprom.c \
-			$(GENPLUS_SRC_DIR)/cart_hw/md_eeprom.c \
+			$(GENPLUS_SRC_DIR)/cart_hw/eeprom_93c.c \
+			$(GENPLUS_SRC_DIR)/cart_hw/eeprom_i2c.c \
+			$(GENPLUS_SRC_DIR)/cart_hw/eeprom_spi.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/ggenie.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/sram.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/svp/ssp16.c \
 			$(GENPLUS_SRC_DIR)/cart_hw/svp/svp.c \
 			$(GENPLUS_SRC_DIR)/ntsc/md_ntsc.c \
 			$(GENPLUS_SRC_DIR)/ntsc/sms_ntsc.c \
-			$(GENPLUS_SRC_DIR)/sound/Fir_Resampler.c \
 			$(GENPLUS_SRC_DIR)/sound/eq.c \
 			$(GENPLUS_SRC_DIR)/sound/sound.c \
 			$(GENPLUS_SRC_DIR)/sound/ym2612.c \
 			$(GENPLUS_SRC_DIR)/sound/ym2413.c \
 			$(GENPLUS_SRC_DIR)/sound/sn76489.c \
-			$(GENPLUS_SRC_DIR)/sound/blip.c \
+			$(GENPLUS_SRC_DIR)/sound/blip_buf.c \
 			$(GENPLUS_SRC_DIR)/z80/z80.c \
 			$(GENPLUS_SRC_DIR)/m68k/m68kcpu.c \
-			$(LIBRETRO_DIR)/libretro.c
+			$(GENPLUS_SRC_DIR)/m68k/s68kcpu.c \
+			$(LIBRETRO_DIR)/libretro.c \
+			$(LIBRETRO_DIR)/scrc32.c
 
 LOCAL_C_INCLUDES = $(LOCAL_PATH)/$(GENPLUS_SRC_DIR) \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/sound \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/input_hw \
+			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/cd_hw \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/cart_hw \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/cart_hw/svp \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/m68k \
@@ -58,5 +67,6 @@ LOCAL_C_INCLUDES = $(LOCAL_PATH)/$(GENPLUS_SRC_DIR) \
 			$(LOCAL_PATH)/$(GENPLUS_SRC_DIR)/ntsc \
 			$(LOCAL_PATH)/$(LIBRETRO_DIR)
 
-LOCAL_CFLAGS = -DINLINE=inline -DUSE_15BPP_RENDERING -DLSB_FIRST -D__LIBRETRO__ 
+LOCAL_CFLAGS = -ffast-math -O3 -funroll-loops -DINLINE="static inline" -DUSE_16BPP_RENDERING -DLSB_FIRST -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565
+
 include $(BUILD_SHARED_LIBRARY)
